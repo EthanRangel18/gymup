87609595c98ce599566731140efd25152d638d09
srjjte <155252422+srjjte@users.noreply.github.com>
2024-06-12 08:25:46 -0600

    Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,20 +1,9 @@
 <?php
-// Definir la función para obtener la URL base dinámica
-function calculate_base_url() {
-    $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http";
-    $host = $_SERVER['HTTP_HOST'];
-    $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-    return $protocol . "://" . $host . $uri . "/";
-}
-
-// Definir una constante utilizando define
-define('base_url', calculate_base_url());
+const base_url = "https://gymup-web-azure.azurewebsites.net/";
 const host = "localhost";
 const user = "root";
 const pass = "";
 const db = "gymup";
 const charset = "charset=utf8";
 
-// Uso de la constante BASE_URL
-echo BASE_URL;
 ?>
