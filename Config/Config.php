b03feac3fa4583f257aa607cbde148e7d7e30226
srjjte <155252422+srjjte@users.noreply.github.com>
2024-06-12 00:13:49 -0600

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -6,8 +6,4 @@ const pass = "";
 const db = "gymup";
 const charset = "charset=utf8";
 
-function base_url1() {
-    return 'https://gymup-web-azure.azurewebsites.net/';  // Reemplaza con la URL base de tu sitio
-}
-
 ?>
